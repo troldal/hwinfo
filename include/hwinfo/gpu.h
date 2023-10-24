5cf4f0ddc896afcb993096766506e9343d54ce0b
Kenneth Troldal Balslev <kenneth.balslev@gmail.com>
2023-10-24 18:15:42 +0200

    Update copyright headers in GPU code files
    Updated copyright headers have been added to multiple GPU related code files. The new headers include an ASCII art logo, credit to original author Leon Freist and addition of new copyright holder Kenneth Troldal Balslev for 2023. The headers also include full license permission specification and warranty disclaimer regarding the usage and distribution of the code.

--- include/hwinfo/gpu.h
+++ include/hwinfo/gpu.h
@@ -1,46 +1,41 @@
-// Copyright Leon Freist
-// Author Leon Freist <freist@informatik.uni-freiburg.de>
+/*
+
+    88        88  I8,        8        ,8I  88                 ad88
+    88        88  `8b       d8b       d8'  88                d8"
+    88        88   "8,     ,8"8,     ,8"   88                88
+    88aaaaaaaa88    Y8     8P Y8     8P    88  8b,dPPYba,  MM88MMM  ,adPPYba,
+    88""""""""88    `8b   d8' `8b   d8'    88  88P'   `"8a   88    a8"     "8a
+    88        88     `8a a8'   `8a a8'     88  88       88   88    8b       d8
+    88        88      `8a8'     `8a8'      88  88       88   88    "8a,   ,a8"
+    88        88       `8'       `8'       88  88       88   88     `"YbbdP"'
+
+    Based on the work of:
+    Leon Freist <freist@informatik.uni-freiburg.de>
+
+    Copyright © 2022 Leon Freist
+    Copyright © 2023 Kenneth Troldal Balslev
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the “Software”), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is furnished
+    to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #pragma once
 
 #include "platform.h"
-
-//#include <cstdint>
-//#include <string>
-//#include <vector>
-//
-//namespace hwinfo {
-//
-//class GPU {
-//  friend std::vector<GPU> getAllGPUs();
-//
-// public:
-//  ~GPU() = default;
-//
-//  HWI_NODISCARD const std::string& vendor() const { return _vendor; }
-//  HWI_NODISCARD const std::string& name() const { return _name; }
-//  HWI_NODISCARD const std::string& driverVersion() const { return _driverVersion; }
-//  HWI_NODISCARD int64_t memory_Bytes() const { return _memory_Bytes; }
-//  HWI_NODISCARD int64_t frequency_MHz() const { return _frequency_MHz; }
-//  HWI_NODISCARD int num_cores() const { return _num_cores; }
-//  HWI_NODISCARD int id() const { return _id; }
-//
-// private:
-//  GPU() = default;
-//  std::string _vendor{};
-//  std::string _name{};
-//  std::string _driverVersion{};
-//  int64_t _memory_Bytes{0};
-//  int64_t _frequency_MHz{0};
-//  int _num_cores{0};
-//  int _id{0};
-//
-//  std::string _vendor_id{};
-//  std::string _device_id{};
-//};
-//
-//std::vector<GPU> getAllGPUs();
-//}  // namespace hwinfo
 
 #if defined(HWINFO_APPLE)
 #include "apple/gpu.h"
--- include/hwinfo/linux/gpu.h
+++ include/hwinfo/linux/gpu.h
@@ -1,16 +1,39 @@
+/*
 
-// Copyright Leon Freist
-// Author Leon Freist <freist@informatik.uni-freiburg.de>
+    88        88  I8,        8        ,8I  88                 ad88
+    88        88  `8b       d8b       d8'  88                d8"
+    88        88   "8,     ,8"8,     ,8"   88                88
+    88aaaaaaaa88    Y8     8P Y8     8P    88  8b,dPPYba,  MM88MMM  ,adPPYba,
+    88""""""""88    `8b   d8' `8b   d8'    88  88P'   `"8a   88    a8"     "8a
+    88        88     `8a a8'   `8a a8'     88  88       88   88    8b       d8
+    88        88      `8a8'     `8a8'      88  88       88   88    "8a,   ,a8"
+    88        88       `8'       `8'       88  88       88   88     `"YbbdP"'
+
+    Based on the work of:
+    Leon Freist <freist@informatik.uni-freiburg.de>
+
+    Copyright © 2022 Leon Freist
+    Copyright © 2023 Kenneth Troldal Balslev
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the “Software”), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is furnished
+    to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include "../base/gpu.h"
-
-// #include "../PCIMapper.h"
-// #include "../gpu.h"
-// #include "../utils/filesystem.h"
-//
-// #include <fstream>
-// #include <string>
-// #include <vector>
 
 namespace hwinfo
 {
